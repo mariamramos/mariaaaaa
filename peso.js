d2c90c8c72d858772aae83344e6245296bf971d9
mariamramos <113263858+mariamramos@users.noreply.github.com>
2023-10-02 12:32:42 -0300

    Add files via upload

---
+++ peso.js
@@ -0,0 +1,28 @@
+//cria referencial ao form
+const form = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//ouvinte
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = form.inNome.value
+    const masculino = form.inMasculino.checked
+    const altura = Number(form.inAltura.value)
+
+    //calculando o peso ideal
+    let peso
+    if (masculino){
+        peso = 22 * Math.pow(altura, 2)
+    } else {
+        peso = 21 * Math.pow(altura, 2)
+    } 
+    // apresenta a resposta
+    resp.innerText = `${nome} seu peso ideal é: ${peso.toFixed(3)}kg`
+})
+
+// segundo ouvinte. limpa o conteudo de h3
+
+form.addEventListener("reset", (e) => {
+    resp.innerText = "" //limpa o conteudo de h3
+})
